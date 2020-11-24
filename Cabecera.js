1ad59c6026ce8e72c6f42dcced74148668adf60a
UO264921 <71704577+UO264921@users.noreply.github.com>
2020-11-24 13:37:26 +0100

    Add files via upload

---
+++ Cabecera.js
@@ -0,0 +1,8 @@
+var asignatura = new Object();
+asignatura.nombre = "Software y estándares en la Web";
+asignatura.titulacion = "Grado en Ingeniería Informática del Software";
+asignatura.centro = "Escuela de Ingeniería Informática";
+asignatura.universidad = "Universidad de Oviedo";
+asignatura.curso = "2020-21";
+asignatura.estudiante = "Iyán Sanjurjo Luna";
+asignatura.email = "uo264921@uniovi.es";
